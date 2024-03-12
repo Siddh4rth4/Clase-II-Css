5b6afba8438cab837f4abb2b5eb94ce0b5d2b558
Siddh4rth4 <andy_garry2@hotmail.com>
2024-03-12 18:50:38 -0500

    Initial commit

---
+++ javas.js
@@ -0,0 +1,20 @@
+//mensaje de consola
+//alert("Marilyn es una buerquita");
+
+//constantes y variables
+
+//let name ="Wepardo";
+
+//var names ="Wepardos";
+
+//const nombre ="Wepardow";
+
+//console.log(name);
+//console.log(names);
+//console.log(nombre);
+
+
+//Asignar variable
+
+let titulo = document.getElementsByClassName("h2");
+console.log(titulo);
